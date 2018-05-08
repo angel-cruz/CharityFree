ad6f24cc251b8b76e544c6fcdd46fb92daddddf1
Angel <goodtimecruz@gmail.com>
2018-05-08 19:01:00 -0400

    Bug free version
    Bug free version total

---
+++ version 3.0/database.php
@@ -0,0 +1,17 @@
+<?php
+//Create connection credentials
+
+$db_host = 'localhost';
+$db_name = 'quizzer';
+$db_user = 'root';
+$db_pass = 'root';
+//default MAMP password is root
+
+//Create mysql object
+$mysqli = new mysqli( $db_host, $db_user, $db_pass, $db_name);
+
+//Error Handler
+if($mysqli->connect_error){
+	printf("connect failed: %s/n", $mysqli->connect_error);
+}
+?>
